61fd4e486643bb8bc9f2cf1cb0e643fd27d3c571
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-05-23 15:03:51 +0700

    Add logout session

--- src/controller/DashboardServlet.java
+++ src/controller/DashboardServlet.java
@@ -11,15 +11,20 @@ import java.io.IOException;
 @WebServlet(name = "DashboardServlet", urlPatterns = "/dashboard")
 public class DashboardServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        logOut(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         showDashboard(request, response);
     }
 
-    public void showDashboard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-            RequestDispatcher dispatcher = request.getRequestDispatcher("admin_interface.jsp");
-            dispatcher.forward(request, response);
+    public void showDashboard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("admin_interface.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    public void logOut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        request.getSession().invalidate();
+        response.sendRedirect(request.getContextPath() + "index.jsp");
     }
 }
